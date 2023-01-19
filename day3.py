26c709ba144ff7d0f9db0186a5cacb9aad9777ad
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2023-01-19 19:47:32 +0100

    solve day3

---
+++ day3.py
@@ -0,0 +1,16 @@
+"""
+
+We have learned enough skills for a simple, but cool, project!
+
+Remember when you were a kid and thought the ideal dinner would just be all your favorite things mixed in a bowl?
+How did that Nutella Mac & Cheese taste? Well - let's come up with a recipe generator to build us an amazing dish for today's evening meal!
+
+"""
+
+food = input("Name a food ")
+plant = input("Name a type of plant ")
+methodOfCooking = input("Name a method of cooking ")
+burnedFood = input("A word that desbribes ruined food ")
+diyItem = input("Name a DIY item")
+
+print(methodOfCooking, food, "with ", burnedFood, plant, " on a bed of ", diyItem)
