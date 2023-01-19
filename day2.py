2438307c68fb7b72aadbbc75960dffc89556bcc9
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2023-01-19 19:29:03 +0100

    solve day2

---
+++ day2.py
@@ -0,0 +1,24 @@
+""" 
+
+Please note: This is a silly example.
+You'd never write your own Phishing code, but it's really important to know what to look for.
+If someone's asking you for this kind of information online - think twice!
+
+"""
+
+print("Definitely not a Phishing Scam")
+print()
+myName = input("What is your name?")
+print("Thanks for logging in")
+print(myName)
+cardNumber = input("What is your credit card number?")
+print("Thanks, I definitely won't put")
+print(cardNumber)
+print("into Amazon and order anything weird")
+print()
+print("Promise")
+maidenName = input("What is your Mother's maiden name? ")
+print()
+print("That's cool, I just wanted to know that")
+print(maidenName)
+print("was your Mum's maiden name. Not because the bank requested it or anything, honest.")
