14a8ecc04cf357a87e4f11631dc2e2b743efd268
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2023-02-12 19:09:09 +0100

    solve day30

---
+++ day30.py
@@ -0,0 +1,10 @@
+print("30 Days Down - What did you think?")
+print()
+
+for i in range(1, 31):
+    thought = input(f"Day {i}:\n")
+    print()
+    myText = f"You thought Day {i} was"
+    print(f"{myText:^35}")
+    print(f"{thought:^35}")
+    print()
